379650ccb5d58fae35d2b63734a55ee90c16a5db
Emil Privér <emil@rivercode.se>
2020-02-03 11:37:32 +0100

    smalöl change to show how git works

---
+++ im_bored/Create.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace im_bored
+{
+    public class Create
+    {
+        public static void CreateFile()
+        {
+            Console.WriteLine("Create class");
+        }
+    }
+}
