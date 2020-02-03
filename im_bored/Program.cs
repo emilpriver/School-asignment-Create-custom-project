17204c028f78014cb6dbac33fc98c1e8bdee389d
Emil Privér <emil@rivercode.se>
2020-02-03 11:24:02 +0100

    init

---
+++ im_bored/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace im_bored
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
